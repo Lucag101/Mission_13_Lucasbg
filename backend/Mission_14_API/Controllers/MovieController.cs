e94d02b3932396965f34d2062fefa57abebb2a78
Lucag101 <114105978+Lucag101@users.noreply.github.com>
2023-04-05 21:28:46 -0600

    changes for mission 14

---
+++ backend/Mission_14_API/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_14_API.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Mission_14_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp)
+        {
+            context = temp;
+        }
+        public IEnumerable<MovieCollection> Get()
+        {
+            var x = context.Movies.ToArray()
+                ;
+            return x;
+        }
+
+
+    }
+}
